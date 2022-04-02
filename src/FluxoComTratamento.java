1253143d485e85b1d0f5fe265e095bd62350d78a
Joao-Gontijo <jvgontijo166@gmail.com>
2022-04-02 16:34:31 -0300

    minhaexceção estendendo exception

--- src/FluxoComTratamento.java
+++ src/FluxoComTratamento.java
@@ -4,21 +4,24 @@ public class FluxoComTratamento {
 		System.out.println("Init do main");
 		try {
 			metodo1();
-		} catch (ArithmeticException | NullPointerException | MinhaExcecao e) {
+		} catch (ArithmeticException | NullPointerException e) {
 			System.out.println(e.getMessage());
+			e.printStackTrace();
+		} catch (MinhaExcecao e) {
+			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		
 		System.out.println("Fim do main");
 	}
 	
-	private static void metodo1() {
+	private static void metodo1() throws MinhaExcecao {
 		System.out.println("Ini do metodo1");
 		metodo2();
 		System.out.println("Fim do metodo1");
 	}
 	
-	private static void metodo2() {
+	private static void metodo2() throws MinhaExcecao {
 		System.out.println("Ini do metodo2");
 		throw new MinhaExcecao("Deu muiro errado");
 		//System.out.println("Fim do metodo2");
